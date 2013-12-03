62fda712dcc64b81f27e5e850162b39751599b69
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-12-03 11:44:00 +0100

    control: refactor and fix the cascade generation algorithm
    The refactoring is creating a separate class, which will allow us
    to use different sets of rules
    
    The fix was to properly type the output of the convertion components.
    Otherwise, there is an ambiguity in connections

--- models/blueprints/control/netgen.rb
+++ models/blueprints/control/netgen.rb
@@ -35,92 +35,129 @@ module RockAUV
             Rule.new("aligned_vel2effort", [:aligned,:vel], [:aligned,:effort], Hash[], AuvControl::PIDController),
             Rule.new("effort_aligned2body", [:aligned,:effort], [:body,:effort], Hash[Axis.new(:x,:y,:z) => Axis.new(:x,:y,:z)], AuvControl::AlignedToBody)]
 
-        Producer = Struct.new :name, :domain, :axis, :srv
+        Producer = Struct.new :name, :domain, :axis, :srv do
+            def to_s
+                "#<Producer/#{domain[0]}/#{domain[1]}/#{axis} #{srv}>"
+            end
+        end
 
         def self.netgen(producers)
-            result = Cascade.new_submodel
-
-            # Add the producers to the composition, so that we deal only with
-            # children of the composition
-            producers = add_producers_to_cascade(result, producers)
-
-            # Sort the producers by reference/quantity they generate
-            producers_by_domains = Hash.new
-            producers.each do |name, raw_producer|
-                resolved_producer = producer_domains(name, raw_producer)
-                resolved_producer.each do |p|
-                    producers_by_domains[p.domain] ||= Array.new
-                    producers_by_domains[p.domain] << p
-                end
-            end
-
-            # Apply the rules one by one, in order
-            NETGEN_RULES.each do |rule|
-                if resolved_producers = producers_by_domains[rule.source_domain]
-                    new_axis, new_producer = apply_rule(result, rule, resolved_producers)
-                    producers_by_domains[rule.target_domain] ||= Array.new
-                    producers_by_domains[rule.target_domain] << Producer.new(rule.name, rule.target_domain, new_axis, new_producer)
-                end
-            end
-            result
+            CascadeGenerator.new(NETGEN_RULES).create(producers)
         end
 
-        def self.add_producers_to_cascade(cascade, producers)
-            result = Hash.new
-            producers.each do |name, p|
-                p = p.to_instance_requirements
-                result[name] = cascade.add p, :as => name
-            end
-            result
-        end
+        # Algorithm that generates a submodel of Cascade based on a set of
+        # producers
+        class CascadeGenerator
+            extend Logger::Hierarchy
+            include Logger::Hierarchy
 
-        # Returns the set of domains a producer controls
-        #
-        # @return [{[Symbol,Symbol] => Producer}]
-        def self.producer_domains(name, producer)
-            srv = producer.find_data_service_from_type(OutputDomainSrv)
-            if !srv
-                raise ArgumentError, "#{producer} has no OutputDomainSrv service"
-            end
-            # We want the actual service, not the service-as-OutputDomainSrv
-            srv = srv.as_real_model
+            # Set of rules this generator should apply
+            #
+            # @return [Array<Rule>]
+            attr_reader :rules
 
-            result = Array.new
-            srv.model.model.domain.each do |reference, quantity, axis|
-                base_srv = OUTPUTS[reference][quantity]
-                result << Producer.new(name, [reference,quantity], axis, srv.as(base_srv))
+            def initialize(rules)
+                @rules = rules
             end
-            result
-        end
 
-        # Applies a given rule on the Cascade submodel
-        #
-        # Given a rule and a set of producers, it adds the relevant children to
-        # the composition model and inserts the convertion element the rule
-        # refers to
-        def self.apply_rule(composition_m, rule, producers)
-            new_axis = Axis.new
+            def create(producers)
+                result = Cascade.new_submodel
 
-            convertion_m = rule.convertion_component.specialize
-            in_reference, in_quantity = *rule.source_domain
-            producer_pairs = producers.map do |p|
-                new_axis |= p.axis
-                [p, convertion_m.require_dynamic_service(
-                    "in_#{in_reference}_#{in_quantity}",
-                    :as => p.name)]
-            end
-            convertion_child = composition_m.add convertion_m, :as => rule.name
-            producer_pairs.each do |p, srv|
-                child_srv = convertion_child.find_data_service(srv.name)
-                p.srv.connect_to child_srv
-            end
-            rule.axis.each do |mask, target|
-                if new_axis & mask != 0
-                    new_axis |= target
+                # Add the producers to the composition, so that we deal only with
+                # children of the composition
+                producers = self.class.add_producers_to_cascade(result, producers)
+
+                # Sort the producers by reference/quantity they generate
+                producers_by_domains = Hash.new
+                producers.each do |name, raw_producer|
+                    resolved_producer = self.class.producer_domains(name, raw_producer)
+                    resolved_producer.each do |p|
+                        producers_by_domains[p.domain] ||= Array.new
+                        producers_by_domains[p.domain] << p
+                    end
                 end
+
+                # Apply the rules one by one, in order
+                rules.each do |rule|
+                    if resolved_producers = producers_by_domains[rule.source_domain]
+                        debug do
+                            debug "applying #{resolved_producers.size} producers"
+                            resolved_producers.each do |p|
+                                debug "  #{p}"
+                            end
+                            break
+                        end
+                        new_axis, new_producer = self.class.apply_rule(result, rule, resolved_producers)
+                        producers_by_domains[rule.target_domain] ||= Array.new
+                        producers_by_domains[rule.target_domain] << Producer.new(rule.name, rule.target_domain, new_axis, new_producer)
+                    end
+                end
+                result
             end
 
-            return new_axis, convertion_child
+            def self.add_producers_to_cascade(cascade, producers)
+                result = Hash.new
+                producers.each do |name, p|
+                    p = p.to_instance_requirements
+                    result[name] = cascade.add p, :as => name
+                end
+                result
+            end
+
+            # Returns the set of domains a producer controls
+            #
+            # @return [{[Symbol,Symbol] => Producer}]
+            def self.producer_domains(name, producer)
+                srv = producer.find_data_service_from_type(OutputDomainSrv)
+                if !srv
+                    raise ArgumentError, "#{producer} has no OutputDomainSrv service"
+                end
+                # We want the actual service, not the service-as-OutputDomainSrv
+                srv = srv.as_real_model
+
+                result = Array.new
+                srv.model.model.domain.each do |reference, quantity, axis|
+                    base_srv = OUTPUTS[reference][quantity]
+                    result << Producer.new(name, [reference,quantity], axis, srv.as(base_srv))
+                end
+                result
+            end
+
+            # Applies a given rule on the Cascade submodel
+            #
+            # Given a rule and a set of producers, it adds the relevant children to
+            # the composition model and inserts the convertion element the rule
+            # refers to
+            def self.apply_rule(composition_m, rule, producers)
+                new_axis = Axis.new
+
+                convertion_m = rule.convertion_component.specialize
+                in_reference, in_quantity = *rule.source_domain
+                producer_pairs = producers.map do |p|
+                    new_axis |= p.axis
+                    [p, convertion_m.require_dynamic_service(
+                        "in_#{in_reference}_#{in_quantity}",
+                        :as => p.name)]
+                end
+
+                reference, quantity = *rule.target_domain
+                output_srv = Control::OUTPUTS[reference][quantity]
+                convertion_m.provides output_srv, :as => 'cmd',
+                    "cmd_out_#{reference}_#{quantity}" => "cmd_out"
+
+                convertion_child = composition_m.add convertion_m, :as => rule.name
+                producer_pairs.each do |p, srv|
+                    child_srv = convertion_child.find_data_service(srv.name)
+                    p.srv.connect_to child_srv
+                end
+                rule.axis.each do |mask, target|
+                    if new_axis & mask != 0
+                        new_axis |= target
+                    end
+                end
+
+                return new_axis, convertion_child.cmd_srv
+            end
         end
     end
 end
